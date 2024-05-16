db8c40fb89eb89ca6771eca0c3108f65e460246f
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-05-17 00:04:16 +0100

    update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-test -f modules.db || echo "Couldn't find modules database, check working directory!" && read -r -n 1
+main() {
+  echo "Installing venv tool..."
+  apt install python3.11-venv
 
-apt install python3.11-venv
+  echo "Creating venv..."
+  python3 -m venv .venv
 
-echo "Creating venv..."
-python3 -m venv .venv
+  echo "Activating venv..."
+  source .venv/bin/activate
 
-echo "Activating venv..."
-source .venv/bin/activate
+  echo "Installing requirements..."
+  pip install -r requirements.txt
 
-echo "Installing requirements..."
-pip install -r requirements.txt
+  echo "Installing gunicorn..."
+  pip install gunicorn
 
-echo "Installing gunicorn..."
-pip install gunicorn
+  echo "Telling Flask it is behind a proxy..."
+  touch proxy
 
-echo "Telling Flask it is behind a proxy..."
-touch proxy
-
-echo "Creating service config..."
-cat > /etc/systemd/system/uon-ce.service <<EOF
+  echo "Creating service config..."
+  cat > /etc/systemd/system/uon-ce.service <<EOF
 [Unit]
 Description=UON CE Gunicorn Daemon
 After=network.target
@@ -40,8 +40,15 @@ PrivateTmp=true
 WantedBy=multi-user.target
 EOF
 
-echo "Enabling service..."
-sudo systemctl enable uon-ce
+  echo "Enabling service..."
+  sudo systemctl enable uon-ce
 
-echo "Starting service..."
-sudo systemctl start uon-ce
+  echo "Starting service..."
+  sudo systemctl start uon-ce
+}
+
+if [[ -f modules.db ]]; then
+  main
+else
+  echo "Couldn't find modules.db, check working directory!"
+fi
