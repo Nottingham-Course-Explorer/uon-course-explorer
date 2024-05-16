9c5a18eeed7ba221275040d45ec6075406643944
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-05-16 18:03:25 +0000

    deployment changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
 apt install python3.11-venv
+
+echo "Creating venv..."
 python3 -m venv .venv
+echo "Activating venv..."
 source .venv/bin/activate
 
+echo "Installing requirements..."
 pip install -r requirements.txt
+
+echo "Telling Flask it is behind a proxy..."
+touch src/proxy
+
+
