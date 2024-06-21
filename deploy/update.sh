637484b6eda12bc227927d577e64a205a619f2c4
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-06-21 20:10:50 +0100

    make update script install requirements

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,6 +4,8 @@ git fetch
 
 if [[ -n "$(git log origin/main ^main)" ]]; then
   git rebase origin/main
+  source "${PWD}"/.venv/bin/activate
+  pip install -r requirements.txt
   sudo systemctl restart uon-ce
 fi
 
