07b365784c0f3268cb16522aee7114fd689d8e93
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-06-25 00:48:01 +0100

    improve error handling in deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 database_file="$HOME/ce.sqlite3"
+bind_address="127.0.0.1:5100"
 
 main() {
   echo "Downloading database..."
-  curl "$1" --progress-bar --output "$database_file"
+  curl "$1" --progress-bar --output "$database_file" || return 1
 
   echo "Installing venv tool..."
-  apt install python3.11-venv
+  apt install python3.11-venv || return 1
 
   echo "Creating venv..."
-  python3 -m venv .venv
+  python3 -m venv .venv || return 1
 
   echo "Activating venv..."
-  source .venv/bin/activate
+  source .venv/bin/activate || return 1
 
   echo "Installing requirements..."
-  pip install -r requirements.txt
+  pip install -r requirements.txt || return 1
 
   echo "Installing gunicorn..."
-  pip install gunicorn
+  pip install gunicorn || return 1
 
   echo "Creating service config..."
-  cat > /etc/systemd/system/uon-ce.service <<EOF
+  cat > /etc/systemd/system/uon-ce.service <<EOF || return 1
 [Unit]
 Description=UON CE Gunicorn Daemon
 After=network.target
@@ -34,7 +35,7 @@ WorkingDirectory=$PWD/src
 Environment="PATH=$PWD/.venv/bin"
 Environment="CE_PROXY='True'"
 Environment="CE_DATABASE=$database_file"
-ExecStart=$PWD/.venv/bin/gunicorn --bind 127.0.0.1:5100 app:app
+ExecStart=$PWD/.venv/bin/gunicorn --bind $bind_address app:app
 ExecReload=/bin/kill -s HUP $MAINPID
 KillMode=mixed
 TimeoutStopSec=5
@@ -45,10 +46,12 @@ WantedBy=multi-user.target
 EOF
 
   echo "Enabling service..."
-  sudo systemctl enable uon-ce
+  sudo systemctl enable uon-ce || return 1
 
   echo "Starting service..."
-  sudo systemctl start uon-ce
+  sudo systemctl start uon-ce || return 1
+
+  echo "Success!"
 }
 
 if [[ $# -eq 1 ]]; then
--- deploy/update_database.sh
+++ deploy/update_database.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-main() {
-  primary_file="$HOME/ce.sqlite3"
-  temp_file="$HOME/ce.sqlite3.tmp"
+primary_file="$HOME/ce.sqlite3"
+temp_file="$HOME/ce.sqlite3.tmp"
 
-  curl "$1" --progress-bar --output "$temp_file" && mv "$temp_file" "$primary_file"
+main() {
+  echo "Downloading database..."
+  curl "$1" --progress-bar --output "$temp_file" && mv "$temp_file" "$primary_file" && echo "Success!"
 }
 
 if [[ $# -eq 1 ]]; then
