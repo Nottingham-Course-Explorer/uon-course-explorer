a6e5c1328e3a144f69ed71d35823f7ea55402ff6
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-06-25 00:55:23 +0100

    display info in update script

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "Fetching changes..."
 git fetch
 
 if [[ -n "$(git log origin/main ^main)" ]]; then
+  echo "Changes detected, updating..."
   git rebase origin/main
   source "${PWD}"/.venv/bin/activate
   pip install -r requirements.txt
   sudo systemctl restart uon-ce
+  echo "Done."
+else
+  echo "No changes detected."
 fi
-
-sudo systemctl is-active uon-ce
