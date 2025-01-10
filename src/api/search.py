18d083a40cf9e74e49540a43e1fe00e2a6d4f962
ntaulbut <ntaulbut@gmail.com>
2025-01-10 19:42:53 +0000

    implement opensearch suggestions

--- src/api/search.py
+++ src/api/search.py
@@ -1,18 +1,36 @@
-from flask import request, Response
+import json
+
+from flask import request, Response, url_for
 
 from tools import get_db, add_column_names_list
 
 
 def opensearch_suggestions():
-    search_terms = request.args.get("search_terms", "")
+    search_term = request.args.get("search_terms", "")
     cursor = get_db().cursor()
     cursor.execute(
-        "SELECT * FROM modules WHERE title LIKE ?", ("%" + search_terms + "%",)
+        "SELECT * FROM modules WHERE title LIKE ? LIMIT 5", ("%" + search_term + "%",)
     )
     modules = add_column_names_list(cursor.fetchall())
 
+    suggestion_texts = []
+    description_texts = []
+    urls = []
+
+    for module in modules:
+        suggestion_texts.append(module["title"])
+        description_texts.append(module["description"])
+        urls.append(url_for("module", code=module["code"]))
+
     response = Response(
-        """["test", ["Just! Stop using Makefile", "What's new in Django 3.2 LTS", "Django Plaintext Password", "Temperature & Humidity Sensor with ESPHome", "State of the Server 2024"]]"""
+        json.dumps(
+            [
+                search_term,
+                suggestion_texts,
+                description_texts,
+                urls
+            ]
+        )
     )
     response.headers["Content-Type"] = "application/x-suggestions+json; charset=utf-8"
     response.headers["Access-Control-Allow-Origin"] = "*"
